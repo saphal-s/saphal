6cefd107d7138bb9a84deda5e65f51f33da28488
saphal-s <subedisaphal0992gmail.com>
2022-06-30 23:58:05 +0545

    Updates

---
+++ precache-manifest.2c93290c78a58e2478df749acad6ded1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3683645c96196d73a120",
+    "url": "/saphal/static/css/main.fac1eb89.chunk.css"
+  },
+  {
+    "revision": "3683645c96196d73a120",
+    "url": "/saphal/static/js/main.3683645c.chunk.js"
+  },
+  {
+    "revision": "5fc6eca808e7c1951395",
+    "url": "/saphal/static/js/1.5fc6eca8.chunk.js"
+  },
+  {
+    "revision": "7a33e4fd72f87a928345",
+    "url": "/saphal/static/css/2.406a875a.chunk.css"
+  },
+  {
+    "revision": "7a33e4fd72f87a928345",
+    "url": "/saphal/static/js/2.7a33e4fd.chunk.js"
+  },
+  {
+    "revision": "b6082281816908024597",
+    "url": "/saphal/static/js/runtime~main.b6082281.js"
+  },
+  {
+    "revision": "6fcd701dfffc04400b9522736c962cae",
+    "url": "/saphal/index.html"
+  }
+];
